0f102ae04beee6e9e5d8752a3e7b381bc4a2730b
bsy1141 <bsy1141@naver.com>
2021-04-20 17:45:52 +0900

    js scroll,transparent

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 'use strict';
 
+function scrollIntoView(selector){
+    const scrollTo=document.querySelector(selector);
+    scrollTo.scrollIntoView({behavior:"smooth"});
+}
+
 // Transparent navbar
 const navbar=document.querySelector('#navbar');
 const navbarHeight=navbar.getBoundingClientRect().height;
@@ -24,12 +29,21 @@ navbarMenu.addEventListener('click',(event)=>{
         return;
     }
 
-    const scrollTo=document.querySelector(link);
-    scrollTo.scrollIntoView({behavior:"smooth"});
+    scrollIntoView(link);
 });
 
+//if "contact me" button is clicked, Scroll to Contact Section
 
+const contactBtn=document.querySelector('.home__contact');
 
+contactBtn.addEventListener('click',()=>{
+    scrollIntoView("#contact");
+})
 
+//Make home slowly fade to transparent as the window scrolls down
+const home=document.querySelector('.home__container');
+const homeHeight=home.getBoundingClientRect().height;
 
-
+window.addEventListener('scroll',()=>{
+    home.style.opacity=1-window.scrollY/homeHeight;
+})
